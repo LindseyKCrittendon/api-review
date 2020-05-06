8c4bf523fca036fcc471b5f28a532811085cb63c
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-05-06 15:16:56 -0400

    urls linked

--- script.js
+++ script.js
@@ -15,15 +15,20 @@ fetch("http://localhost:8088/restaurants")
            //Print restaurants to DOM
            console.log(parsedRestaurants[0])
            document.querySelector("#restaurantNameContainer").innerHTML += 
-           `<h4>${restaurant.restaurant.name}</h4>
+           `<a href="${restaurant.restaurant.url}" target="_blank"><h4>${restaurant.restaurant.name}</h4></a>
            <p>${restaurant.restaurant.location.address}</p>
            <p>Rating ${restaurant.restaurant.user_rating.aggregate_rating}</p>
-           <p>Average cost for two $${restaurant.restaurant.average_cost_for_two}</p>`;
+           <p>Average cost for two $${restaurant.restaurant.average_cost_for_two}</p>
+           <a href="${restaurant.restaurant.menu_url}" target="_blank"><button>View Menu</button></a>`;
         console.log(restaurant.restaurant.average_cost_for_two)
         })
     });
 
 //     UI/ UX enhancements before you go on to stretch goals:
 // Display the restaurant's address, aggregate user rating, and average cost for in addition to its name
+//done
+
 // Make each restaurant's name a hyperlink to their website
+
+
 // Style each restaurant as a card, and add a card to each button that says "View Menu". This button should be a hyperlink to the restaurant's menu.
